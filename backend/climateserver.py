6719959bc08bc7c1504afa13f1bee9fd131df7be
vilmelhina <vilhelmina2.0@gmail.com>
2024-01-14 02:15:33 +0100

    Enabled CORS on climate server

--- backend/climateserver.py
+++ backend/climateserver.py
@@ -1,17 +1,24 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 from climate import Climate
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 climate = Climate()
 
 @app.route('/temperature', methods=["GET"])
+@cross_origin()
 def get_temperature():
     return jsonify(climate.get_temperature())
 
 @app.route('/pressure', methods=["GET"])
+@cross_origin()
 def get_pressure():
     return jsonify(climate.get_pressure())
 
 @app.route('/humidity', methods=["GET"])
+@cross_origin()
 def get_humidity():
     return jsonify(climate.get_humidity())
