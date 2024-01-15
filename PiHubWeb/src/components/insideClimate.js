6e17d42ba378d8af5f6fe49535b30e06d64da855
vilmelhina <vilhelmina2.0@gmail.com>
2024-01-15 23:43:01 +0100

    Rounded inside climate numbers and removed pressure

--- PiHubWeb/src/components/insideClimate.js
+++ PiHubWeb/src/components/insideClimate.js
@@ -1,15 +1,13 @@
 import React from "react";
 import {useRecoilValue} from "recoil";
-import {insideHumidityState, insidePressureState, insideTemperatureState} from "../model/atoms";
+import {insideHumidityState, insideTemperatureState} from "../model/atoms";
 
-export function InsideClimate(props) {
+export function InsideClimate() {
     const insideTemperature = useRecoilValue(insideTemperatureState)
     const humidity = useRecoilValue(insideHumidityState)
-    const pressure = useRecoilValue(insidePressureState)
 
     return <span>
-            <p>Temperatur: {insideTemperature}</p>
-            <p>Luftfuktighet: {humidity}</p>
-            <p>Lufttryck: {pressure}</p>
+            <p>Temperatur: {Math.round(insideTemperature)}</p>
+            <p>Luftfuktighet: {Math.round(humidity)}</p>
     </span>
 }
