24bfdd9e02430297414d3dd76c850111d238b70b
vilmelhina <vilhelmina2.0@gmail.com>
2024-01-19 14:39:43 +0100

    Display minutes left until departure

--- PiHubWeb/src/components/departure.js
+++ PiHubWeb/src/components/departure.js
@@ -3,20 +3,27 @@ import "../styles/departure.css";
 
 export function Departure(props) {
     const departure = props.departure;
+    const hasNewTime = departure.rtTime && departure.rtTime !== departure.time
+    const time = departure.rtTime || departure.time
+    const date = departure.rtDate || departure.date
+    const timeLeft =  (Date.parse(date + " " + time) - Date.now()) / (1000 * 60)
 
-    function hasNewTime(departure) {
-        return departure.rtTime && departure.rtTime !== departure.time
+    function displayTimeLeft() {
+        if(timeLeft >= 1)
+            return Math.floor(timeLeft) + " min"
+        else
+            return "nu"
     }
 
-    return <div className="departure">
+    return timeLeft > -0.7 && <div className="departure">
         <span className="departure-line">{departure.line}</span>
         <span className="departure-direction">{departure.direction}</span>
         <span className="departure-time">
-                        <span className={"original-time" + (hasNewTime(departure) ? " corrected" : "")}>
-                            {departure.time.substring(0,5)}
-                        </span>
-            {hasNewTime(departure) && <span className="new-time">{departure.rtTime.substring(0,5)}</span> }
+            <span className={"original-time" + (hasNewTime ? " corrected" : "")}>
+                {departure.time.substring(0,5)}
+            </span>
+            {hasNewTime && <span className="new-time"> {departure.rtTime.substring(0,5)}</span> }
         </span>
-        <span className="departure-time-left">1 min</span>
+        <span className="departure-time-left">{displayTimeLeft()}</span>
     </div>
 }
